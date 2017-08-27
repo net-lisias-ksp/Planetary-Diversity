efbfd0288f89bee8d3c05f110df2940723e9a2b5
Dorian Stoll <dorian@tmsp.io>
2017-08-27 14:17:56 +0200

    Better Seed Selection

--- src/main/SeedParams.cs
+++ src/main/SeedParams.cs
@@ -1,66 +1,94 @@
-﻿/*using System;
+﻿using System;
 using KSP.Localization;
 using UnityEngine;
+using System.Linq;
+using System.Reflection;
 
 namespace PlanetaryDiversity
 {
     /// <summary>
     /// Gives the user an opportunity to set the seed of their savegame
     /// </summary>
-    public class SeedParams : GameParameters.CustomParameterNode
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public class SeedParams : MonoBehaviour
     {
-        private PopupDialog dialog;
-
-        [GameParameters.CustomParameterUI("#LOC_PlanetaryDiversity_SeedParams_Seed", newGameOnly = true)]
-        public Boolean seedSetter
-        {
-            get { return false; }
-            set
-            {
-                if (!value || dialog != null)
-                    return;
-                if (Seed == null)
-                    Seed = new System.Random(Environment.TickCount ^ Guid.NewGuid().GetHashCode()).Next().ToString();
-                dialog = PopupDialog.SpawnPopupDialog(new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), new MultiOptionDialog("SeedParams",
-                    Localizer.Format("#LOC_PlanetaryDiversity_SeedParams_DialogMsg"),
-                    Localizer.Format("#LOC_PlanetaryDiversity_SeedParams_DialogTitle"),
-                    UISkinManager.GetSkin("MainMenuSkin"),
-                    new DialogGUITextInput(
-                        Seed, false, 10000, (s) => { Seed = s; return s; }, 24f
-                    ),
-                    new DialogGUIButton(
-                        Localizer.Format("#LOC_PlanetaryDiversity_SeedParams_DialogOK"), () => { dialog.Dismiss(); }, false
-                    )
-                    ), true, UISkinManager.GetSkin("MainMenuSkin"));
-            }
-        }
-        
+        /// <summary>
+        /// The seed for the new game
+        /// </summary>
         public String Seed { get; set; }
 
-        public override String Title => Localizer.Format("#LOC_PlanetaryDiversity_SeedParams_Title");
+        /// <summary>
+        /// The main menu Instance
+        /// </summary>
+        private MainMenu menu { get; set; }
 
-        public override String Section => "Advanced";
-
-        public override Int32 SectionOrder => 3;
-
-        public override Boolean HasPresets => false;
-
-        public override GameParameters.GameMode GameMode => GameParameters.GameMode.ANY;
-
-        public override String DisplaySection => "#autoLoc_6002170";
-
-        public SeedParams() : base()
+        /// <summary>
+        /// Gets called when the mono behaviour is created and registers a callback for changing
+        /// the game seed
+        /// </summary>
+        void Awake()
         {
             GameEvents.onGameStateCreated.Add(OnGameStateCreated);
+            DontDestroyOnLoad(this);
+        }
+
+        /// <summary>
+        /// Adds a hook into the MainMenu UI for editing the New Game Dialog
+        /// </summary>
+        void Start()
+        {
+            menu = FindObjectsOfType<MainMenu>().FirstOrDefault();
+            menu.newGameBtn.onTap += OnNewGameBtnTap;
+        }
+
+        /// <summary>
+        /// This function gets called when the user clicks the "New Game" button in the main menu
+        /// </summary>
+        void OnNewGameBtnTap()
+        {
+            FieldInfo createGameDialog = typeof(MainMenu).GetFields(BindingFlags.NonPublic | BindingFlags.Instance).FirstOrDefault(f => f.FieldType == typeof(PopupDialog));
+            if (createGameDialog == null)
+                return;
+            PopupDialog dialog = createGameDialog.GetValue(menu) as PopupDialog;
+            if (dialog == null)
+                return;
+            if (dialog.dialogToDisplay == null)
+                return;
+            DialogGUIHorizontalLayout d1 = dialog.dialogToDisplay.Options[0] as DialogGUIHorizontalLayout;
+            if (d1 == null)
+                return;
+            DialogGUIVerticalLayout d2 = d1.children[0] as DialogGUIVerticalLayout;
+            if (d2 == null)
+                return;
+
+            // Create the new layout
+            DialogGUIHorizontalLayout layout = new DialogGUIHorizontalLayout(new DialogGUIBase[]
+            {
+                new DialogGUILabel(Localizer.Format("#LOC_PlanetaryDiversity_SeedParams_Seed"), true, false),
+                new DialogGUIFlexibleSpace(),
+                new DialogGUITextInput(Seed ?? "", Localizer.Format("#LOC_PlanetaryDiversity_SeedParams_Placeholder"), false, 32, (s) => Seed = s, 200f, 30f)
+            });
+            d2.children.Insert(1, layout);
+            d1.children[0] = d2;
+            dialog.dialogToDisplay.Options[0] = d1;
+            PopupDialog newDialog = PopupDialog.SpawnPopupDialog(new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), dialog.dialogToDisplay, false, menu.guiSkinDef.SkinDef, true, "");
+            dialog.Dismiss();
+            createGameDialog.SetValue(menu, newDialog);
         }
 
         void OnGameStateCreated(Game game)
         {
-            if (Seed == null)
-                game.Seed = new System.Random(Environment.TickCount ^ Guid.NewGuid().GetHashCode()).Next();
-            if (!Int32.TryParse(Seed, out game.Seed))
+            Seed = Seed?.Trim();
+            Debug.Log(Seed);
+            Debug.Log(game.Seed);
+            if (String.IsNullOrEmpty(Seed))
+                return;
+            if (Int32.TryParse(Seed, out Int32 iSeed))
+                game.Seed = iSeed;
+            else
                 game.Seed = Seed.GetHashCode();
+            Debug.Log(game.Seed);
             Seed = null;
         }
     }
-}*/
+}
