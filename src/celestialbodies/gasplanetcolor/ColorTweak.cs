09397457408f034693eedf00ef3393883b29b6d3
Dorian Stoll <dorian.stoll@tmsp.io>
2017-09-23 19:52:49 +0200

    gas planet recolor

---
+++ src/celestialbodies/gasplanetcolor/ColorTweak.cs
@@ -0,0 +1,63 @@
+﻿using PlanetaryDiversity.API;
+using System;
+using UnityEngine;
+
+namespace PlanetaryDiversity.CelestialBodies.GasPlanetColor
+{
+    /// <summary>
+    /// Changes the colors of Gas Planets
+    /// </summary>
+    public class ColorTweak : CelestialBodyTweaker
+    {
+        /// <summary>
+        /// Returns the name of the config node that stores the configuration
+        /// </summary>
+        public override String GetConfig() => "PD_CELESTIAL";
+
+        /// <summary>
+        /// Returns the name of the config option that can be used to disable the tweak
+        /// </summary>
+        public override String GetSetting() => "GasPlanetColor";
+
+        /// <summary>
+        /// Changes the parameters of the body
+        /// </summary>
+        public override Boolean Tweak(CelestialBody body)
+        {
+            // Is the body a GasPlanet?
+            if (body.hasSolidSurface || body.scaledBody.GetComponentsInChildren<SunShaderController>().Length != 0)
+                return false;
+
+            // Tweak the color (this only has a chance of 50% to happen
+            if (GetRandom(HighLogic.CurrentGame.Seed, 0, 100) < 50)
+            {
+
+                // Get the material of the body
+                Material material = body.scaledBody.GetComponent<Renderer>().sharedMaterial;
+
+                // Get the average color of the current texture
+                Texture2D diffuseMap = Utility.CreateReadable((Texture2D)material.GetTexture("_MainTex"));
+                Color average = Utility.GetAverageColor(diffuseMap);
+
+                // Select a new color and apply it
+                Color newColor = GetRandomElement(HighLogic.CurrentGame.Seed, Utility.colors);
+                material.color = Utility.ReColor(newColor, average);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Makes a color a bit different
+        /// </summary>
+        private Color AlterColor(Color c)
+        {
+            return new Color((Single)Math.Min(1, c.r * GetRandomDouble(HighLogic.CurrentGame.Seed, 0.92, 1.05)),
+                             (Single)Math.Min(1, c.g * GetRandomDouble(HighLogic.CurrentGame.Seed, 0.92, 1.05)),
+                             (Single)Math.Min(1, c.b * GetRandomDouble(HighLogic.CurrentGame.Seed, 0.92, 1.05)), c.a);
+        }
+    }
+}
---
+++ src/celestialbodies/gasplanetcolor/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PlanetaryDiversity.CelestialBodies.GasPlanetColor")]
+[assembly: AssemblyDescription("Modifies the color of Gas Planets.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PlanetaryDiversity.CelestialBodies.GasPlanetColor")]
+[assembly: AssemblyCopyright("Copyright © Dorian Stoll 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5d13838e-40e3-43c0-b9b2-29c843db876a")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: KSPAssembly("PlanetaryDiversity.CelestialBodies.GasPlanetColor", 1, 0)]
+[assembly: KSPAssemblyDependency("PlanetaryDiversity.API", 1, 0)]
